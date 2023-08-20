babfc74e2f5a0a439304544806a283a657dad978
Lesuuda <lesuudaljamani@gmail.com>
2023-08-19 20:11:37 -0700

    update3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__mmain__":
+if __name__ == "__main__":
 
     """print the number of and lists of arguments"""
 
@@ -8,7 +8,7 @@ if __name__ == "__mmain__":
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):
