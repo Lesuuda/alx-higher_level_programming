d77d522526400d9eed944228b0d980543616c374
lesuuda <lesuudaljamani@gmail.com>
2023-11-30 19:19:31 +0000

    number 3 fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,13 @@
 import sys
 if __name__ == "__main__":
 
-
     """adds infinite arguments together"""
 
     count = len(sys.argv)
+    if count == 1:
+        total = 0
     if count > 1:
         total = 0
-    
     for arg in sys.argv[1:]:
         total += int(arg)
     print(total)
-
