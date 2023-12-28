5346e34e602413af04f5e603369d4cdc4d2f9e53
lesuuda <lesuudaljamani@gmail.com>
2023-12-28 19:42:47 +0000

    number 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+
+    """
+    function that divides 2 integers and prints the result.
+    """
+
+    try:
+        result = a / b
+    except (ZeroDivisionError):
+        result = None
+    except Exception as e:
+        result = Nonei
+    else:
+        return result
+    finally:
+        print("Inside result: {}".format(result))
