7ce317d2e69a91f486d65df0f2fe11c545cb23c4
lesuuda <lesuudaljamani@gmail.com>
2023-12-28 18:40:54 +0000

    number 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+
+    """
+    handles ValueError and TypeError
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
