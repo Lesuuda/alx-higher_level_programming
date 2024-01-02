6849a731c8753bff325c8671c89458c4750805d7
lesuuda <lesuudaljamani@gmail.com>
2024-01-02 05:30:39 -0800

    number 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Defines a square based on 1-square.py"""
+
+
+class Square:
+    """Defines a square class"""
+    def __init__(self, size=0):
+        """__init__ instatiation
+        Attributes:
+                  size(int): size of the new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """ Returns the area of a square"""
+        area = (self.__size * self.__size)
+        return area
