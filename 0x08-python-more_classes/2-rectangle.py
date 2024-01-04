3f7115ad7f47d99af6b728bcca713ba4f2db74a8
lesuuda <lesuudaljamani@gmail.com>
2024-01-04 07:13:07 -0800

    number 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class Rectangle:
+    '''an empty class that defines a rectangle'''
+    def __init__(self, width=0, height=0):
+        """instatiation
+        Attributes:
+                  height(int): height of rectangle
+                  width(int): width of rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """private instance attribute width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """private instance attribute height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """gets the rectangle area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if self.width and self.height == 0:
+            perimeter == 0
+        return 2 * (self.width + self.height)
