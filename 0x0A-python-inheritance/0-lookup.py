c14d090362a5ebf65224c648377b38d3cb103171
lesuuda <lesuudaljamani@gmail.com>
2024-01-08 12:13:05 +0000

    number 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that prints the list of attributes of an object"""
+
+
+def lookup(obj):
+    """uses the dir() function to print the list of attributes
+    in an object"""
+    return dir(obj)
