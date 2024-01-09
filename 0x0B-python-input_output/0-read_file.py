30c6729010558d1cb703cf67d38e955034087f66
lesuuda <lesuudaljamani@gmail.com>
2024-01-09 08:12:00 +0000

    a

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """opens the file using the with statement"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        content = f.read()
+        return content
