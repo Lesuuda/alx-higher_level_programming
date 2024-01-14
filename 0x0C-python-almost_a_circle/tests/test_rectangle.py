7ac59a6395a4f5dc52240c6ea5ab6885ceda43c9
lesuuda <lesuudaljamani@gmail.com>
2024-01-14 04:04:33 +0000

    tests revamp

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,75 +1,49 @@
 #!/usr/bin/python3
-"""tests for class rectangle"""
+"""test cases for class rectangle"""
 
 
 import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase):
-    """tests for class rectangle"""
-    def test_width_getter_setter(self):
-        """tests for the effectiveness of width getter and setter"""
-        r1 = Rectangle(width=5, height=10)
-        r1.width = 15
-        self.assertEqual(r1.width, 15)
+class TestRectangle_width(unittest.TestCase):
+    """unittests for testing the width attribute"""
+    def test_none_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(None, 2)
 
-    def test_height_getter_setter(self):
-        """tests for the effectiveness of height getter and setter"""
-        r1 = Rectangle(width=5, height=10)
-        r1.height = 15
-        self.assertEqual(r1.height, 15)
+    def test_str_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("width", 2)
 
-    def test_x_getter_setter(self):
-        """tests for the effectiveness of x getter and setter"""
-        r1 = Rectangle(10, 2, 0, 0, 12)
-        r1.x = 15
-        self.assertEqual(r1.x, 15)
+    def test_float_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(2.2, 2)
 
-    def test_y_getter_setter(self):
-        """tests for the effectiveness of y getter and setter"""
-        r1 = Rectangle(10, 2, 3, 4, 12)
-        r1.y = 15
-        self.assertEqual(r1.y, 15)
+    def test_list_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle([1, 2, 3], 2)
+    
+    def test_tuple_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle({1, 2, 3}, 2)
 
-    def test_id_setter(self):
-        """tests for effectiveness of id setter"""
-        r1 = Rectangle(10, 2, 3, 4, 12)
-        r1.id = 100
-        self.assertEqual(r1.id, 100)
+    def test_complex_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(complex(3), 2)
 
-    def test_width_negative_value(self):
-        """tets for width negative value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(width=-5, height=10, x=3, y=4, id=12)
-        self.assertIn("width must be > 0", str(context.exception))
+    def test_range_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle(range(6), 2)
 
-    def test_height_nagative_value(self):
-        """tets for height negative value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(5, -10, 3, 4, 12)
-        self.assertIn("height must be > 0", str(context.exception))
+    def test_dict_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle({"a": 3, "b": 6}, 2)
 
-    def test_x_nagative_value(self):
-        """tets for x negative value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(5, 10, -3, 4, 12)
-        self.assertIn("x must be >= 0", str(context.exception))
+    def test_zero_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 2)
 
-    def test_y_nagative_value(self):
-        """tets for y negative value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(5, 10, 3, -4, 12)
-        self.assertIn("y must be >= 0", str(context.exception))
-
-    def test_width_zero_value(self):
-        """tets for width zero  value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(0, 10, 3, 4, 12)
-        self.assertIn("width must be > 0", str(context.exception))
-
-    def test_height_zero_value(self):
-        """tets for height negative value"""
-        with self.assertRaises(ValueError) as context:
-            r1 = Rectangle(5, 0, 3, 4, 12)
-        self.assertIn("height must be > 0", str(context.exception))
+    def test_negative_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(-3, 2)
