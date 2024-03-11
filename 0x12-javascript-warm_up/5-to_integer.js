55ea009a178a5b16de5290a931d75ca977610000
lesuuda <lesuudaljamani@gmail.com>
2024-03-11 17:35:57 +0300

    sevaral mandataory solutions

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arg1 = process.argv[2];
+const convertedNumber = parseInt(arg1);
+if (!isNaN(convertedNumber)) {
+  console.log(convertedNumber);
+} else {
+  console.log('Not a number');
+}
