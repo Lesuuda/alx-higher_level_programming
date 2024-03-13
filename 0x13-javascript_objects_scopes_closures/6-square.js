eef719de43ae36e3fb1e01c2c6cb356a286ebe4c
lesuuda <lesuudaljamani@gmail.com>
2024-03-13 23:07:37 +0300

    some mandatory tasks

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const SquareI = require('./5-square');
+class Square extends SquareI {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let i = 0; i < this.height; i++) {
+        let r = '';
+        for (let j = 0; j < this.width; j++) {
+          r += c;
+        }
+        console.log(r);
+      }
+    }
+  }
+}
+module.exports = Square;
