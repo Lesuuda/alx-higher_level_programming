fff105a437d5c072027e715e6c29a0b3f819f997
lesuuda <lesuudaljamani@gmail.com>
2024-03-14 11:23:10 +0300

    correction for 6-square.js, corrected position of paranthesis for if block

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const SquareI = require('./5-square');
+const SquareI = require('./5-square.js');
 class Square extends SquareI {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
-      for (let i = 0; i < this.height; i++) {
-        let r = '';
-        for (let j = 0; j < this.width; j++) {
-          r += c;
-        }
-        console.log(r);
+    }
+    for (let i = 0; i < this.height; i++) {
+      let r = '';
+      for (let j = 0; j < this.width; j++) {
+        r += c;
       }
+      console.log(r);
     }
   }
 }
