516b14b88f0c6e122ab1b6cc0fe4c5e440ae88de
lesuuda <lesuudaljamani@gmail.com>
2024-03-16 15:34:05 +0300

    created base class state that inherits from Basfrom base of declaratibe base()

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 lists all states from the database hbtn_0e_0_usa
-that has the same name as the 4th argument
+that start with 'N'
 """
 import MySQLdb
 import sys
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         print(row)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+from sqlalchemy import Column, String, Integer, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    states class that inherits from the base class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
