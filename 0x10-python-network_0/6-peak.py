df25794d266bc3d254828d82ad4ceb6ae368334d
lesuuda <lesuudaljamani@gmail.com>
2024-03-27 17:23:40 +0300

    fixed pycodestyle for 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""returns the peak value in an array"""
+
+
 def find_peak(list_of_integers):
+    """returns the peak value in an array"""
     if list_of_integers == []:
         return None
     list_of_integers = sorted(list_of_integers)
